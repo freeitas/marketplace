68c164112cbfa61c05ea905e95b74eed67b15a13
Guilherme Freitas <freeeeeitas@gmail.com>
2020-03-04 22:58:15 -0300

    Header e Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>eu vou me matar kk</p>
+  </footer>
+</template>
+<script>
+export default {
+  name: "TheFooter"
+};
+</script>
+<style>
+footer {
+  background: #87f;
+  padding: 25px 0px;
+  text-align: center;
+  font-size: 1.2rem;
+  color: #fff;
+  position: relative;
+  top: 75px;
+}
+</style>
