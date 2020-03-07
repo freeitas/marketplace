8652cf698ae8fd163ccdaaf88b4577bb6505c87b
Guilherme Freitas <freeeeeitas@gmail.com>
2020-03-07 09:21:08 -0300

    ¨busca¨

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,9 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <form action="">
+      <input name="busca" id="busca" type="text" v-model="busca" />
+      <input
+        type="submit"
+        id="lupa"
+        value="Buscar"
+        @click.prevent="buscarProdutos"
+      />
+      {{ busca }}
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ProdutosBuscar"
+  name: "ProdutosBuscar",
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
 };
 </script>
--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="produtos-container">
     <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0].src"
-        :alt="produto.fotos[0].titulo"
-      />
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
       <p class="preco">{{ produto.preco }}</p>
       <h2 class="titulo">{{ produto.nome }}</h2>
       <p>{{ produto.descricao }}</p>
@@ -15,19 +11,32 @@
 
 <script>
 import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
 
 export default {
   name: "ProdutosLista",
   data() {
     return {
-      produtos: null
+      produtos: null,
+      produtosPorPagina: 9
     };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    }
   },
   methods: {
     getProdutos() {
-      api.get("http://localhost:3001/produto").then(r => {
+      api.get(this.url).then(r => {
         this.produtos = r.data;
       });
+    }
+  },
+  watch: {
+    url() {
+      this.getProdutos();
     }
   },
   created() {
