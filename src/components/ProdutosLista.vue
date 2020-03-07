c6580f724a83662e51afa54a927cbeae76d0c01a
Guilherme Freitas <freeeeeitas@gmail.com>
2020-03-07 00:23:48 -0300

    Produtos

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,7 +1,33 @@
-<template></template>
+<template>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo" />
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.descricao}}</p>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
-  name: "ProdutosLista"
+  name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch("http://localhost:3001/produto")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
 };
 </script>
