b0223b5b100767f3abecc7681be74e7f7786710c
Guilherme Freitas <freeeeeitas@gmail.com>
2020-03-22 22:08:56 -0300

    Transacao

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -1,11 +1,13 @@
 <template>
   <form action>
-    <label for="nome">Nome</label>
-    <input id="nome" name="nome" type="text" v-model="nome" />
-    <label for="email">Email</label>
-    <input id="email" name="email" type="email" v-model="email" />
-    <label for="senha">Senha</label>
-    <input id="senha" name="senha" type="password" v-model="senha" />
+    <div class="usuario" v-if="mostrarDadosLogin">
+      <label for="nome">Nome</label>
+      <input id="nome" name="nome" type="text" v-model="nome" />
+      <label for="email">Email</label>
+      <input id="email" name="email" type="email" v-model="email" />
+      <label for="senha">Senha</label>
+      <input id="senha" name="senha" type="password" v-model="senha" />
+    </div>
     <label for="rua">Rua</label>
     <input id="rua" name="rua" type="text" v-model="rua" />
     <label for="cep">Cep</label>
@@ -45,7 +47,10 @@ export default {
       ],
       base: "usuario",
       mutation: "UPDATE_USUARIO"
-    })
+    }),
+    mostrarDadosLogin() {
+      return !this.$store.state.login || this.$route.name === "usuario-editar";
+    }
   },
   methods: {
     preencherCep() {
@@ -64,10 +69,15 @@ export default {
 </script>
 
 <style scoped>
-form {
+form,
+.usuario {
   display: grid;
   grid-template-columns: 80px 1fr;
   align-items: center;
+}
+
+.usuario {
+  grid-column: 1 / 3;
 }
 
 .button {
--- src/views/Produto.vue
+++ src/views/Produto.vue
@@ -10,8 +10,11 @@
         <h1>{{produto.nome}}</h1>
         <p class="preco">{{produto.preco | numeroPreco}}</p>
         <p class="descricao">{{produto.descricao}}</p>
-        <button class="btn" v-if="produto.vendido === 'false'">Comprar</button>
-        <button class="btn" v-else>Produto Vendido</button>
+        <transition mode="out-in" v-if="produto.vendido === 'false'">
+          <button class="btn" v-if="!finalizar" @click="finalizar = true">Comprar</button>
+          <FinalizarCompra v-else :produto="produto" />
+        </transition>
+        <button class="btn" v-else disabled>Produto Vendido</button>
       </div>
     </div>
     <PaginaCarregando v-else />
@@ -19,13 +22,18 @@
 </template>
 <script>
 import { api } from "@/services.js";
+import FinalizarCompra from "@/components/FinalizarCompra.vue";
 
 export default {
   name: "Produtos",
   props: ["id"],
+  components: {
+    FinalizarCompra
+  },
   data() {
     return {
-      produto: null
+      produto: null,
+      finalizar: false
     };
   },
   methods: {
